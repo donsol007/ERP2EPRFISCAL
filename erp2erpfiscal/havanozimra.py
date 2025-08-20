90a9d284f99ba6cbdaa6025082067b8798b86c11
donsol007 <donsol007@gmail.com>
2025-08-20 19:40:53 +0100

    Send update

--- erp2erpfiscal/havanozimra.py
+++ erp2erpfiscal/havanozimra.py
@@ -128,7 +128,7 @@ def send_invoice_to_cloud(
         #print("Payload:", data)
         with httpx.Client() as client:
             response =  client.post(url, data=data, headers=headers)
-            frappe.log_error("Invoice Status",f"{devicesn} Invoice Successfully Sent")
+            frappe.log_error("Invoice Status",f"{devicesn} Invoice Successfully Sent Response: {response.text}")
             return response.text
     
     except Exception as e:
